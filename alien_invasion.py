91edf4705be770095b9f89238347998de9b4d2c9
Антон Тарасов <fatalklg@yandex.ru>
2024-01-11 00:28:13 +0300

    Добавлена нарастающая сложность игры.

--- alien_invasion.py
+++ alien_invasion.py
@@ -71,6 +71,8 @@ class AlienInvasion():
 		"""Запускает новую игру при нажатии кнопки Play."""
 		button_clicked = self.play_button.rect.collidepoint(mouse_pos)
 		if button_clicked and not self.stats.game_active:
+			# Сброс игровых настроек.
+			self.settings.initialize_dynamic_settings()
 			self._start_game()
 	
 	def _start_game(self):
@@ -149,6 +151,7 @@ class AlienInvasion():
 		if not self.aliens:
 			self.bullets.empty()
 			self._create_fleet()
+			self.settings.increase_speed()
 
 	def _create_fleet(self):
 		"""Создание флота пришельцев."""
--- settings.py
+++ settings.py
@@ -12,7 +12,6 @@ class Settings():
 		self.bg_color = (230, 230, 230)
 
 		# Настройки корабля.
-		self.ship_speed = 1.5
 		self.ship_limit = 2
 
 		# Параметры снаряда.
@@ -23,12 +22,28 @@ class Settings():
 		self.bullets_allowed = 3
 
 		# Настройки пришельцев.
-		self.alien_speed = 1.0
-		self.fleet_drop_speed = 10
-		# fleet_direction = 1 обозначает движение вправо, а -1 - влево.
-		self.fleet_direction = 1
+		self.fleet_drop_speed = 15
 
 		# Настройки капель воды.
 		self.drop_speed = 1.0
 		self.drop_width = 10
 		self.drop_height = 15
+
+		# Темп ускорения игры.
+		self.speedup_scale = 1.1
+
+		self.initialize_dynamic_settings()
+
+	def initialize_dynamic_settings(self):
+		"""Инициализирует настройки, меняющиеся во время игры."""
+		self.ship_speed = 1.5
+		self.bullet_speed = 3.0
+		self.alien_speed = 1.0
+
+		self.fleet_direction = 1
+
+	def increase_speed(self):
+		"""Увеличивает настройки скорости."""
+		self.ship_speed *= self.speedup_scale
+		self.bullet_speed *= self.speedup_scale
+		self.alien_speed *= self.speedup_scale
